baa0c14937ccf19b1149ba2273ddbc0a9ff6bd7a
sammy wlodawski <samwlodawski@gmail.com>
2024-08-17 20:29:54 -0400

    Added create category post route and get all categories get route.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,16 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const newCategory = await Category.findAll();
   // find all categories
   // be sure to include its associated Products
+    res.status(200).json(newCategory);
+  } catch (err) {
+    res.status (400).json(err);
+  }
+
 });
 
 router.get('/:id', (req, res) => {
@@ -13,8 +20,15 @@ router.get('/:id', (req, res) => {
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create(req.body);
+
+    res.status(200).json(newCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
