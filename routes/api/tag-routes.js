779a608d7d53e490c686eff196baf2f4f9f5e81a
sammy wlodawski <samwlodawski@gmail.com>
2024-08-21 18:42:22 -0400

    Added route to update tag by id and delete tag by id.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { where } = require('sequelize');
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
@@ -21,22 +22,57 @@ router.get('/:id', async(req, res) => {
     const tagData = await Tag.findByPk(req.params.id, {
       include: [{model: 'Product'}]
     });
+    if (!tagData) {
+      res.status(404).json({message: 'This tag does not exist'});
+      return
+    }
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.post('/', (req, res) => {
   // create a new tag
+router.post('/', async (req, res) => {
+try {
+  const newTagData = await Tag.create(req.body);
+  res.status(200).json(newTagData)
+} catch (err) {
+} res.status(500).json(err);
 });
 
-router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+   try {
+    const [updatedRows] = await Tag.update(req.body, {
+      where: {id: req.params.id},
+    });
+    if(updatedRows===0) {
+      return res.status(404).json({message: 'This tag does not exist'});
+    }
+    const updatedTag = await Tag.findByPk(req.params.id);
+    res.status(200).json(updatedTag)
+   } catch (err) {
+    res.status(500).json(err)
+   }
 });
 
-router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deleteRows = await Tag.destroy(req.params.id, {
+      where: {
+        id: req.params.id
+      }
+    });
+    if (deleteRows===0) {
+      res.status(404).json({message: 'This tag does not exist'});
+      return
+    } 
+    res.status(200).json({message: 'Tag has been deleted'});
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
