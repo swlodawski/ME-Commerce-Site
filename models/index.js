96a5a9f372c163f62a2850f09c1360a0e321fbbd
sammy wlodawski <samwlodawski@gmail.com>
2024-08-23 18:23:07 -0400

    Filled in the required sequelize models code.

--- models/index.js
+++ models/index.js
@@ -5,13 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'id', onDelete: 'CASCADE',
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_id',
+  through: ProductTag,
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+})
 module.exports = {
   Product,
   Category,
