4b1bf992d943b24fdba50a8c86e3bd0f5c9b96d5
sammy wlodawski <samwlodawski@gmail.com>
2024-08-27 11:48:24 -0400

    Rechanged route file.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
     const categoryData = await Category.findAll({
     include: [Product]
     });
-    res.status(200).json(JSON.parse(categoryData));
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status (500).json(err);
   }
